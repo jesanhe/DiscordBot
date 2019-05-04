702802502159899157417797c3ff0ead4ce738d2
Jero Sancho <jerosancho@gmail.com>
2019-05-04 03:40:10 +0200

    env variables updated

--- index.js
+++ index.js
@@ -1,115 +1,121 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 // const config = require("./config.json");
 
-client.on('ready', () => {
+client
+  .login(process.env.discord_token)
+  .then(console.log)
+  .catch(console.error);
+
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log(process.env)
 
   let guild = client.guilds;
   // console.log(guild);
-//   let channels = guild.channels;
-//   console.log(channels.array());
+  //   let channels = guild.channels;
+  //   console.log(channels.array());
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!");
   }
-  if (msg.content === 'init') {
+  if (msg.content === "init") {
     console.log(msg.guild.channels);
     msg.guild.channels.forEach(channel => {
       console.log(channel.name);
-      if (channel.name === 'logs'){
-        
+      if (channel.name === "logs") {
       }
-    })
+    });
   }
 });
 
-client.login(process.env.disord_token);
+client.on("voiceStateUpdate", (oldMember, newMember) => {
+  let newUserChannel = newMember.voiceChannel;
+  let oldUserChannel = oldMember.voiceChannel;
 
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-    let newUserChannel = newMember.voiceChannel
-    let oldUserChannel = oldMember.voiceChannel
+  let now = new Date();
+  now = formatDate(now);
 
-    let now = new Date();
-    now = formatDate(now);
+  // console.log("**********");
+  // console.log("*        *");
+  // console.log("*  LOGS  *");
+  // console.log("*        *");
+  // console.log("**********");
 
-    console.log('**********')
-    console.log('*        *')
-    console.log('*  LOGS  *')
-    console.log('*        *')
-    console.log('**********')
+  // console.log('old user')
+  // console.log(oldMember.guild.channels);
+  // console.log('new member');
+  // console.log(newMember);
+  // TODO: wait until yhe channel is created to start sending message
+  isLogCreated(oldMember);
+  const channel = oldMember.guild.channels.find(ch => ch.name === "logs");
+  // console.log(channel);
+  // console.log(newUserChannel.name);
 
-    // console.log('old user')
-    // console.log(oldMember.guild.channels);
-    // console.log('new member');
-    // console.log(newMember);
-    // TODO: wait until yhe channel is created to start sending message
-    isLogCreated(oldMember);
-    const channel = oldMember.guild.channels.find(ch => ch.name === 'logs');
-    // console.log(channel);
-    // console.log(newUserChannel.name);
+  // TODO: Convertirlo en un switch case con todas las posibles condiciones, user conect, user disconect, user switch chanel
 
-    // TODO: Convertirlo en un switch case con todas las posibles condiciones, user conect, user disconect, user switch chanel
-  
-    if(oldUserChannel === undefined && newUserChannel !== undefined) {
-       // User Joins a voice channel
-      
-       channel.send(`[${now}] - ${newMember.user.username.toUpperCase()} joined VoiceChanel ${newUserChannel.name.toUpperCase()}`);
-  
-    } else if(newUserChannel === undefined && oldUserChannel !== undefined) {
-      // User leaves a voice channel
+  if (oldUserChannel === undefined && newUserChannel !== undefined) {
+    // User Joins a voice channel
 
-      channel.send(`[${now}] - ${oldMember.user.username.toUpperCase()} left VoiceChanel ${oldUserChannel.name.toUpperCase()}`);
-  
-    } else if(oldUserChannel !== undefined && newUserChannel !== undefined) {
-      // User switch voice channel
+    channel.send(
+      `[${now}] - ${newMember.user.username.toUpperCase()} joined VoiceChanel ${newUserChannel.name.toUpperCase()}`
+    );
+  } else if (newUserChannel === undefined && oldUserChannel !== undefined) {
+    // User leaves a voice channel
 
-      channel.send(`[${now}] - ${oldMember.user.username.toUpperCase()} switched VoiceChanel from ${oldUserChannel.name.toUpperCase()} to ${newUserChannel.name.toUpperCase()}`);
+    channel.send(
+      `[${now}] - ${oldMember.user.username.toUpperCase()} left VoiceChanel ${oldUserChannel.name.toUpperCase()}`
+    );
+  } else if (oldUserChannel !== undefined && newUserChannel !== undefined) {
+    // User switch voice channel
 
+    channel.send(
+      `[${now}] - ${oldMember.user.username.toUpperCase()} switched VoiceChanel from ${oldUserChannel.name.toUpperCase()} to ${newUserChannel.name.toUpperCase()}`
+    );
+  }
+
+  // let cosas = `[${now.getHours()}:${now.getMinutes()} - ${now.getDay()}/${now.getMonth()}/${now.getFullYear()}]`;
+});
+
+function isLogCreated(member) {
+  let logs = false;
+  member.guild.channels.forEach(channel => {
+    if (channel.type === "text") {
+      if (channel.name === "logs") {
+        logs = true;
+      }
     }
-
-    // let cosas = `[${now.getHours()}:${now.getMinutes()} - ${now.getDay()}/${now.getMonth()}/${now.getFullYear()}]`;
   });
 
-  function isLogCreated(member) {
-    let logs = false;
-    member.guild.channels.forEach(channel => {
-
-      if(channel.type === 'text') {
-        if(channel.name === 'logs') {
-          logs = true;
-        }
-      }
-    });
-
-    if(!logs) {
-      // TODO: Create the channel in solo read mode
-      member.guild.createChannel('logs', 'text').then(console.log).catch(console.error);
-    }
-  };
-
-  function formatDate(date) {
-    let hour = date.getHours();
-    let minutes = date.getMinutes();
-    let day = date.getDay();
-    let month = date.getMonth();
-    let year = date.getFullYear();
-
-    hour = twoDigits(hour);
-    minutes = twoDigits(minutes);
-    day = twoDigits(day);
-    month = twoDigits(month);
-
-    function twoDigits(data) {
-      data = data.toString();
-      if (data.length < 2) {
-        data = `0${data}`;
-      }
-      return data;
-    }
-
-    return `${hour}:${minutes} - ${day}/${month}/${year}`;
+  if (!logs) {
+    // TODO: Create the channel in solo read mode
+    member.guild
+      .createChannel("logs", "text")
+      .then(console.log)
+      .catch(console.error);
   }
+}
+
+function formatDate(date) {
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+
+  hour = twoDigits(hour);
+  minutes = twoDigits(minutes);
+  day = twoDigits(day);
+  month = twoDigits(month);
+
+  function twoDigits(data) {
+    data = data.toString();
+    if (data.length < 2) {
+      data = `0${data}`;
+    }
+    return data;
+  }
+
+  return `${hour}:${minutes} - ${day}/${month}/${year}`;
+}
